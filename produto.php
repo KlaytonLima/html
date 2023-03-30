9af15f211bc7482202d406b047da2ec70c3b26f8
Klayton <github.com/klaytontestegit>
2023-03-30 17:15:38 -0300

    adicionando mais arquivos

---
+++ produto.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/mobile.css" media="(max-width: 320px)">
+    <title>Mirror Fashion</title>
+</head>
+<body>
+    <?php include("cabecalho.php"); ?>
+    <div>
+        <h1>Fuzzy Cardigan</h1>
+        <p>por apenas R$ 129,00</p>
+
+        <form>
+            <fieldset class="cores">
+                <legend>Escolha a cor:</legend>
+
+                <input type="radio" name="cor" value="verde" checked>
+                <label for="verde">
+                    <img src="img/foto2-verde.png" alt="verde">
+                </label>
+                <input type="radio" name="cor" value="verde" checked>
+                <label for="verde">
+                    <img src="img/foto2-azul.png" alt="verde">
+                </label>
+                <input type="radio" name="cor" value="azul" checked>
+                <label for="verde">
+                    <img src="img/foto2-rosa.png" alt="rosa">
+                </label>
+
+            </fieldset>
+
+            <input type="submit" class="comprar" value="Comprar">
+        </form>
+    </div>
+    <?php include("rodape.php"); ?>
+</body>
+</html>
